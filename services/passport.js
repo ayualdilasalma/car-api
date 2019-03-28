0726943ebf6c6e5a6e39d32d1541ff8ac801cfae
Ayu Aldila Salma <ayu.aldilas@yahoo.com>
2019-03-28 16:53:42 +0800

    configure user

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -9,8 +11,17 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        console.log(refreshToken);
-        console.log(profile);
+        const { id } = profile;
+        User.findOne({ googleId: id })
+            .then( (existingUser) => {
+                if ( existingUser ) {
+                    // we have a re
+                } else {
+            new User({
+                googleId: id
+            }).save();
+                }
+            });
+        
     })
 );
